8f4f20f494c230445880c7ad29897e8ffbb6a92a
dsshimel <dsshimel@gmail.com>
2017-11-23 14:39:30 -0500

    Added an example of applying a function to an array, cleaned up code

--- simple_webcam_capture.py
+++ simple_webcam_capture.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy
 
 # Based on http://www.steinm.com/blog/motion-detection-webcam-python-opencv-differential-images/
 
@@ -7,45 +8,41 @@ def diff_img(frame0, frame1, frame2):
   diff1 = cv2.absdiff(frame1, frame2)
   return cv2.bitwise_and(diff0, diff1)
 
-# VideoCapture
+def threshold_max(x):
+  return ((x + 255) / 2) if x > 15 else 0
+
+def read_camera(capture, as_bw):
+  did_read, result = capture.read()
+  if not did_read:
+    return None
+  return cv2.cvtColor(result, cv2.COLOR_RGB2GRAY) if as_bw else result  
+
 capture = cv2.VideoCapture(0)
 
-ret_minus2, image_minus2 = capture.read()
-ret_minus1, image_minus1 = capture.read()
+# Example of what is read from the camera
+# 'image' is a numpy.ndarray
 ret, image = capture.read()
 height, width, channels = image.shape
 print('height: ' + str(height))
 print('width: ' + str(width))
 print('channels: ' + str(channels))
+print('image cell value data type: ' + str(image[0][0].dtype))
 
-t_minus2 = cv2.cvtColor(image_minus2, cv2.COLOR_RGB2GRAY)
-t_minus1 = cv2.cvtColor(image_minus1, cv2.COLOR_RGB2GRAY)
-t = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-# t_minus2 = image_minus2
-# t_minus1 = image_minus1
-# t = image
+t_minus2 = read_camera(capture, as_bw=True)
+t_minus1 = read_camera(capture, as_bw=True)
+t = read_camera(capture, as_bw=True)
 
 while True:
-  # 'image' is a numpy.ndarray
-  # ret, image = capture.read()
-  # bwImage = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-
-  
-  # scale = 640.0 / width
-  # print('scale: ' + str(scale))  
-  # image = cv2.resize(image, (0,0), fx=scale, fy=scale)
-
-  # cv2.imshow('Camera stream', image)
-  # cv2.imshow('Camera stream', bwImage)
   result = diff_img(t_minus2, t_minus1, t)
-  kSize = (3, 3)
-  resultBlur = cv2.blur(result, kSize)
-  cv2.imshow('Camera stream', resultBlur)
+  k_size = (3, 3)
+  result_blur = cv2.blur(result, k_size)
+  vector_func = numpy.vectorize(threshold_max, otypes=[numpy.uint8])
+  transformed_result = vector_func(result_blur)
+  cv2.imshow('Camera stream', transformed_result)
 
   t_minus2 = t_minus1
   t_minus1 = t
-  t = cv2.cvtColor(capture.read()[1], cv2.COLOR_RGB2GRAY)
-  # t = capture.read()[1]
+  t = read_camera(capture, as_bw=True)
 
   # Poll at 1 ms rate for keys to stop loop
   if cv2.waitKey(1) & 0xFF == ord('q'):
