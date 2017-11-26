d09402478162c9e929941f3ee75d922a28fbd928
dsshimel <dsshimel@gmail.com>
2017-11-26 13:50:58 -0500

    Start adding websockets

--- simple_webcam_capture.py
+++ simple_webcam_capture.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 import traceback
 
 # Based on http://www.steinm.com/blog/motion-detection-webcam-python-opencv-differential-images/
@@ -23,70 +24,83 @@ def read_camera(capture, as_bw):
     return None
   return convert_to_bw(result) if as_bw else result  
 
-capture = cv2.VideoCapture(0)
-if not capture.isOpened():
-  capture = cv2.VideoCapture(1)
-if not capture.isOpened():
-  print('Could not open camera on channel 0 or 1')
-  exit()
+def run():
+  capture = cv2.VideoCapture(0)
+  if not capture.isOpened():
+    capture = cv2.VideoCapture(1)
+  if not capture.isOpened():
+    print('Could not open camera on channel 0 or 1')
+    exit()
 
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920);
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080);
+  capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920);
+  capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080);
 
-# Example of what is read from the camera
-# 'image' is a np.ndarray
-ret, image = capture.read()
-height, width, channels = image.shape
-print('height: ' + str(height))
-print('width: ' + str(width))
-print('channels: ' + str(channels))
-print('image cell value data type: ' + str(image[0][0].dtype))
+  # Example of what is read from the camera
+  # 'image' is a np.ndarray
+  ret, image = capture.read()
+  height, width, channels = image.shape
+  print('height: ' + str(height))
+  print('width: ' + str(width))
+  print('channels: ' + str(channels))
+  print('image cell value data type: ' + str(image[0][0].dtype))
 
-t_minus2 = read_camera(capture, as_bw=True)
-t_minus1 = read_camera(capture, as_bw=True)
-t_color = read_camera(capture, as_bw=False)
-t = convert_to_bw(t_color)
+  t_minus2 = read_camera(capture, as_bw=True)
+  t_minus1 = read_camera(capture, as_bw=True)
+  t_color = read_camera(capture, as_bw=False)
+  t = convert_to_bw(t_color)
 
-try:
-  while True:
-    result = diff_img(t_minus2, t_minus1, t)
-    k_size = (3, 3)
-    result_blur = cv2.blur(result, k_size)
-    # vector_func = np.vectorize(threshold_max, otypes=[np.uint8])
-    did_thresh, threshold_result = cv2.threshold(result_blur, 31, 1, cv2.THRESH_BINARY)
-    
-    # transformed_result is only of length 2
-    # nested arrays are length 640 
-    # transformed_result = vector_func(result_blur)
+  frame_count = 0
+  second_start = time.time()
+  try:
+    while True:
+      result = diff_img(t_minus2, t_minus1, t)
+      k_size = (3, 3)
+      result_blur = cv2.blur(result, k_size)
+      # vector_func = np.vectorize(threshold_max, otypes=[np.uint8])
+      did_thresh, threshold_result = cv2.threshold(result_blur, 31, 1, cv2.THRESH_BINARY)
+      
+      # transformed_result is only of length 2
+      # nested arrays are length 640 
+      # transformed_result = vector_func(result_blur)
 
-    # Calculate the x and y moments
-    # This point is what the eye will look at
-    moms = cv2.moments(threshold_result)
-    m0 = moms['m00']
-    m_x, m_y = None, None
-    if m0 > 0:
-      m_x = int(moms['m10'] / m0)
-      m_y = int(moms['m01'] / m0)
-      # print(str(m_x) + ', ' + str(m_y))
+      # Calculate the x and y moments
+      # This point is what the eye will look at
+      moms = cv2.moments(threshold_result)
+      m0 = moms['m00']
+      m_x, m_y = None, None
+      if m0 > 0:
+        m_x = int(moms['m10'] / m0)
+        m_y = int(moms['m01'] / m0)
+        # print(str(m_x) + ', ' + str(m_y))
 
-    im2, contours, hierarchy = cv2.findContours(threshold_result, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(t_color, contours, -1, (0, 255, 0), 1)
+      im2, contours, hierarchy = cv2.findContours(threshold_result, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+      cv2.drawContours(t_color, contours, -1, (0, 255, 0), 1)
 
-    if m_x and m_y:
-      cv2.circle(t_color, (m_x, m_y), 10, (0, 0, 255), 1)
+      if m_x and m_y:
+        cv2.circle(t_color, (m_x, m_y), 10, (0, 0, 255), 1)
 
-    cv2.imshow('Camera stream', t_color)
+      cv2.imshow('Camera stream', t_color)
 
-    t_minus2 = t_minus1
-    t_minus1 = t
-    t_color = read_camera(capture, as_bw=False)
-    t = convert_to_bw(t_color)
+      t_minus2 = t_minus1
+      t_minus1 = t
+      t_color = read_camera(capture, as_bw=False)
+      t = convert_to_bw(t_color)
 
-    # Poll at 1 ms rate for keys to stop loop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
-except Exception as e:
-  traceback.print_exc()
+      # Poll at 1 ms rate for keys to stop loop
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-capture.release()
-cv2.destroyAllWindows()
+      # Frame rate on my laptop is about 30 FPS
+      frame_count += 1
+      if time.time() - second_start > 1:
+        # print('frames per second: ' + str(frame_count))
+        frame_count = 0
+        second_start = time.time()
+  except Exception as e:
+    traceback.print_exc()
+
+  capture.release()
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+  run()
---
+++ websockets_server.py
@@ -0,0 +1,3 @@
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+
+# based on https://www.smashingmagazine.com/2016/02/simple-augmented-reality-with-opencv-a-three-js/#3-websockets-in-both-front-end-and-back-end
