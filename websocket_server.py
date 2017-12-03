5567a0df5153b5e12c53cb1f91b199aef18cd7a2
dsshimel <dsshimel@gmail.com>
2017-12-03 13:25:44 -0500

    Go back to waiting for a client if they disconnect

--- websocket_server.py
+++ websocket_server.py
@@ -24,24 +24,36 @@ class MotionReporterSocket(WebSocket):
 
 def run_server():
   global server
-  # empirically determined
+  # empirically determined by measuring the webcam's output
   frame_rate = 30.0
-  server = SimpleWebSocketServer(host='', port=7777, websocketclass=MotionReporterSocket, selectInterval=1.0 / frame_rate)
+  server = SimpleWebSocketServer(
+      host='', port=7777, websocketclass=MotionReporterSocket, selectInterval=1.0 / frame_rate)
   print('server is serving forever')
   server.serveforever()
 
 t = threading.Thread(target=run_server)
 t.start()
 
-while not client:
-  print('waiting for client to connect')
-  time.sleep(1)
+# Have to use CTRL+Break to get out of this loop.
+# Maybe there's a more graceful way to this?
+try:
+  while True:
+    while not client:
+      print('waiting for client to connect')
+      time.sleep(1)
 
-print('client connected')
+    print('client connected')
 
-md = MotionDetector(client)
-md.run()
+    md = MotionDetector(client)
+    md.run()
+    client.close()
+    client = None
+except Exception as e:
+  traceback.print_exc()
 
+if client:
+  print('closing client')
+  client.close()
 if server:
   print('closing server')
   server.close()
